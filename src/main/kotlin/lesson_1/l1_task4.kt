d9748f74f542a53e86f7d0949ae806e5cd35ce7f
Bai <jumamillion@gmail.com>
2024-01-29 18:59:46 +0600

    l1-task4-variables

---
+++ src/main/kotlin/lesson_1/l1_task4.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    val length: Double = 408686.0000
+    val age: Byte = 27
+    val partOfDay: Float  = 0.075f
+    val second: Short = 6480
+    val partOfYear: Float = 2.0547945205479453E-4f
+    val apogei: Short = 32700
+
+    println(message = "Длина расстояний: $length")
+    println(message = "Возраст: $age")
+    println(message = "Часть дня: $partOfDay")
+    println(message = "Секунда: $second")
+    println(message = "Часть года: $partOfYear")
+    println(message = "Апогей орбиты: $apogei")
+
+
+
+    /*Данные для задачи:
+    Расстояние 40_868_600_000
+    Возраст – 27
+    Часть дня – 0.075
+    Секунды – 6480
+    Часть года – 2.0547945205479453E-4
+    Апогей орбиты – 327000
+    */
+}
