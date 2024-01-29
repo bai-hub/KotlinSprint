e117d1bdd17d3f8e6d948b5ea53c180724d0bd2b
Bai <jumamillion@gmail.com>
2024-01-29 19:42:32 +0600

    l2-task2-ariphmetics

--- src/main/kotlin/lesson_2/l2_task2.kt
+++ src/main/kotlin/lesson_2/l2_task2.kt
@@ -1,12 +1,12 @@
 fun main() {
     val employers = 50
     val salary = 30000
-    val trainee = 30
+    val trainee = 20
     val traineeSalary = 20000
     val expensesSalaryEmployers = employers * salary
     val expensesToTrainee = trainee * traineeSalary
     val generalExpenses = expensesSalaryEmployers + expensesToTrainee
-    val avarageSalaryEmployer = (expensesSalaryEmployers + expensesToTrainee) / 80
+    val avarageSalaryEmployer = (expensesSalaryEmployers + expensesToTrainee) / 70
     println("Расходы на выплат зп сотрудников : $expensesSalaryEmployers")
     println("Общие расходы после прихода стажеров:  $generalExpenses")
     println("Средняя з/п сотрудника после прихода стажеров $avarageSalaryEmployer")
