72e648f50a25cc2adac03f3aef250f777fe3cc2e
Bai <jumamillion@gmail.com>
2024-01-29 18:53:24 +0600

    l1-task3-variables

---
+++ src/main/kotlin/lesson_1/l1_task3.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val flightYear = 1961
+    var hour = 9
+    var minute = 7
+    println("Год полета: ${flightYear}")
+    println("Час полета: ${hour}")
+    println("Минута полета: ${minute}")
+    hour = 10
+    minute = 55
+    print("Время посадки: ${hour}:${minute}")
+
+}
