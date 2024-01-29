43a4b1777e8c5fd0d51b42ecd928e452374fbb0d
Bai <jumamillion@gmail.com>
2024-01-29 19:34:38 +0600

    l1-task2-variables

---
+++ src/main/kotlin/lesson_1/l1_task2.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val numberOfOrder = 32
+    val gratitude = "Спасибо за Покупку!"
+    println("Количество заказа: ${numberOfOrder}")
+    println(gratitude)
+
+    var numberOfEmployers = 200
+//    println("количество сотрудников: ${numberOfEmployers}")
+
+    numberOfEmployers--
+    println("количество сотрудников после уволнение: ${numberOfEmployers}")
+}
