810c8eb59634b725609c54401d40cdbd81869a33
Bai <jumamillion@gmail.com>
2024-01-29 19:49:06 +0600

    l3-task1-string

---
+++ src/main/kotlin/lesson_3/l3_task1.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var greeting: String
+    greeting = "Guten Tag!"
+    val userName = "Ivan"
+    println("$greeting $userName")
+    greeting = "Guten Abend!"
+    println("$greeting $userName")
+    //если вас неправильно понял вас то прошу прощения сенсей!!!
+}
